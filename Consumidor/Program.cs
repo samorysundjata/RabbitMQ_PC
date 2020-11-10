c25db14c873af00136c51455dc18adede15b0eea
Samory Sundjata <samory.sundjata@gmail.com>
2020-11-10 18:15:35 -0300

    Modificando o principal.

--- Consumidor/Program.cs
+++ Consumidor/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace Consumidor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Private Methods
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "filaMQ",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "filaMQ",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Aperte [enter] para sair.");
+                Console.ReadLine();
+            }
         }
+
+        #endregion Private Methods
     }
 }
--- Produtor/Program.cs
+++ Produtor/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace Produtor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Private Methods
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "filaMQ",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                int count = 0;
+                while (true)
+                {
+                    string message = $"{count++} Salve a todos!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "filaMQ",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                    System.Threading.Thread.Sleep(200);
+                }
+            }
         }
+
+        #endregion Private Methods
     }
 }
